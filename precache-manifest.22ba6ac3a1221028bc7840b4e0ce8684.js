e26ef72d1143559b24b21df2856bb07ca4faf85a
pepe199111 <piotrpustul1@gmail.com>
2020-07-16 15:33:14 +0100

    Updates

---
+++ precache-manifest.22ba6ac3a1221028bc7840b4e0ce8684.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cbf65d9ca2ad4132b87a2eb31e80bbf",
+    "url": "/ReactRounting_simply-page/index.html"
+  },
+  {
+    "revision": "f148a0f183d773d29e75",
+    "url": "/ReactRounting_simply-page/static/css/main.fc011130.chunk.css"
+  },
+  {
+    "revision": "1e81419955807cf6852f",
+    "url": "/ReactRounting_simply-page/static/js/2.62ebb666.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactRounting_simply-page/static/js/2.62ebb666.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f148a0f183d773d29e75",
+    "url": "/ReactRounting_simply-page/static/js/main.4a6a5e81.chunk.js"
+  },
+  {
+    "revision": "aa98930f9f466ce11353",
+    "url": "/ReactRounting_simply-page/static/js/runtime-main.77c2445c.js"
+  },
+  {
+    "revision": "cab7beae2c63322b4a1041af278342bd",
+    "url": "/ReactRounting_simply-page/static/media/header1.cab7beae.jpg"
+  },
+  {
+    "revision": "bd639f7057decea8cb8e98fbcec98dd7",
+    "url": "/ReactRounting_simply-page/static/media/header2.bd639f70.jpg"
+  },
+  {
+    "revision": "1cbc01643cee81c53662c6d443cece77",
+    "url": "/ReactRounting_simply-page/static/media/header3.1cbc0164.jpg"
+  }
+]);
